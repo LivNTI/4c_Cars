9a047887b726da09017e8f4a09f293d19032a45c
liv.sundman <liv.sundman@ga.ntig.se>
2024-10-01 10:28:36 +0200

    Created the car class and gave the attributes values

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    //attribute
+    String color; // No standard value is given
+    int speed = 0; // is given speed 0 if nothing else is given
+    String name;
+    int maxSpeed;
+
+
+    //public void methodName(inputparamters){}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Working with Cars");
+
+        //Datatype variableName = create the object
+        //Car      car1         = new Car();
+
+        //create a car
+        Car car1 = new Car();
+
+        //give the car attributes values
+        car1.name = "Hudson";
+        car1.color = "Blue";
+        car1.maxSpeed = 200;
+
+        //create a second car
+        Car car2 = new Car();
+
+        //give the second car attributes values
+        car2.name = "Hudson";
+        car2.color = "Blue";
+        car2.maxSpeed = 200;
+
     }
+
 }

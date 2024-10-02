9fa58f55fcd89226e06bdc24ab35fae29a4c2b17
liv.sundman <liv.sundman@ga.ntig.se>
2024-10-02 10:32:07 +0200

    quick fix of access modifers example with ArrayList

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,36 @@
 public class Car {
     //attribute
-    String color; // No standard value is given
-    int speed = 0; // is given speed 0 if nothing else is given
-    String name;
-    int maxSpeed;
+    private String color; // No standard value is given
+    private int speed = 0; // is given speed 0 if nothing else is given
+    private String name;
+    private int maxSpeed;
 
+    //constructor
+    public Car(String incomingName, String incomingColor, int incominMaxSpeed){
+        name = incomingName;
+        color = incomingColor;
+        maxSpeed = incominMaxSpeed;
+    }
 
-    //public void methodName(inputparamters){}
+    //methods
+    public void speedUp(int change){
+        if (speed + change < maxSpeed){
+            speed = speed + change;
+            System.out.println(name + " now has speed of " + speed);
+        } else{
+            System.out.println("You cannot exceed you rax speed");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Working with Cars");
@@ -6,21 +8,26 @@ public class Main {
         //Car      car1         = new Car();
 
         //create a car
-        Car car1 = new Car();
-
-        //give the car attributes values
-        car1.name = "Hudson";
-        car1.color = "Blue";
-        car1.maxSpeed = 200;
+        Car car1 = new Car("Hudson","Blue",200);
 
         //create a second car
-        Car car2 = new Car();
+        Car car2 = new Car("Mater", "Brown", 100);
 
-        //give the second car attributes values
-        car2.name = "Hudson";
-        car2.color = "Blue";
-        car2.maxSpeed = 200;
+        System.out.println(car1.getName() + " is "+ car1.getColor() + " and has max Speed of " + car1.getMaxSpeed());
+        System.out.println(car2.getName() + " is "+ car2.getColor() + " and has max Speed of " + car2.getMaxSpeed());
 
+        car1.speedUp(115);
+        car2.speedUp(200);
+
+        //Car[] myCars = new Car[10]; //this is a static array
+        ArrayList<Car> myCars = new ArrayList<>();
+        //add the cars to the list
+        myCars.add(car1);
+        myCars.add(car2);
+        myCars.add(new Car("Lightning McQueen", "red", 400));
+
+        Car theCar = myCars.get(2);
+        System.out.println(theCar.getName() + " is "+ theCar.getColor() + " and has max Speed of " + theCar.getMaxSpeed());
     }
 
 }
